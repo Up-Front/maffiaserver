40edfd7eaac6ba6fea25de0ef5659054b8e3ea09
Steven Straatemans <steven@straatemans.nl>
2017-12-01 11:51:14 +0100

    first contact

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var path = require('path');
-server.listen(8090);
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+var path = require("path");
 
-app.get('/', function (req, res) {
-    console.log(path.resolve('/client/index.html'));
-    res.sendfile(path.resolve('/client/index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.resolve("./client/index.html"));
 });
 
-io.on('connection', function (socket) {
-    console.log('connected', socket);
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
+io.on("connection", function(socket) {
+  console.log("Client connected...");
+
+  io.on("join", function(data) {
+    console.log("dd", data);
+  });
 });
+
+server.listen(4200);
