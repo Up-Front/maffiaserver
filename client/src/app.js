873513aff71e2c1f25d7e5708e326a2f2e55bff0
Stefan Lemmen <stefan@up-front.nl>
2017-12-01 14:58:26 +0100

    make it working again

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,12 @@
 import io from "socket.io-client";
 
-const socket = io.connect("http://192.168.1.68:4200");
+const socket = io.connect("http://localhost:4200");
 const loginButton = document.getElementById("login");
 const username = document.getElementById("username");
 
 loginButton.addEventListener("click", loginUser);
 
-function loginUser() {
+function loginUser(e) {
+    e.preventDefault();
     socket.emit("login", username.value);
 }
