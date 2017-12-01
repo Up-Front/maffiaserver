9bcec5f9a5bc1bd15e6567c3f0db9cc714c1a7a0
Stefan Lemmen <stefan@up-front.nl>
2017-12-01 14:30:56 +0100

    optimize

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,11 @@
 import io from "socket.io-client";
 
 const socket = io.connect("http://192.168.1.68:4200");
-const loginUser = e => {
-  const username = document.getElementById("username");
-  socket.emit("login", username.value);
-};
-
 const loginButton = document.getElementById("login");
+const username = document.getElementById("username");
+
 loginButton.addEventListener("click", loginUser);
+
+function loginUser() {
+    socket.emit("login", username.value);
+}
